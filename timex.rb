f328b5763bc7af93e5bab6163d7fcffac7010e13
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2024-01-29 21:36:32 +0100

    feat: skip sections

--- timex.rb
+++ timex.rb
@@ -15,7 +15,12 @@ def parse_options
     sound_theme: nil,
     wrist_app: nil,
     serial_device: "/dev/ttyACM0",
-    verbose: false
+    verbose: false,
+    no_appointments: false,
+    no_anniversaries: false,
+    no_phone_numbers: false,
+    no_lists: false,
+    no_alarms: false
   }
 
   OptionParser.new do |opts|
@@ -36,6 +41,26 @@ def parse_options
     opts.on("--verbose", "Enable verbose mode") do
       options[:verbose] = true
     end
+
+    opts.on("--no-appointments", "Skip creating appointments") do
+      options[:no_appointments] = true
+    end
+
+    opts.on("--no-anniversaries", "Skip creating anniversaries") do
+      options[:no_anniversaries] = true
+    end
+
+    opts.on("--no-phone-numbers", "Skip creating phone numbers") do
+      options[:no_phone_numbers] = true
+    end
+
+    opts.on("--no-lists", "Skip creating lists") do
+      options[:no_lists] = true
+    end
+
+    opts.on("--no-alarms", "Skip creating alarms") do
+      options[:no_alarms] = true
+    end
   end.parse!
 
   options[:json_file] = ARGV[0] # The remaining argument should be the JSON file
@@ -53,37 +78,50 @@ options = parse_options
 data = parse_json_data(options[:json_file])
 
 # Create appointments
-appointments = data["appointments"].map do |appointment|
-  TimexDatalinkClient::Protocol3::Eeprom::Appointment.new(
-    time: Time.parse(appointment["time"]),
-    message: appointment["message"]
-  )
+appointments = []
+if data["appointments"] && !options[:no_appointments]
+  appointments = data["appointments"].map do |appointment|
+    TimexDatalinkClient::Protocol3::Eeprom::Appointment.new(
+      time: Time.parse(appointment["time"]),
+      message: appointment["message"]
+    )
+  end
 end
 
 # Create anniversaries
-anniversaries = data["anniversaries"].map do |anniversary|
-  TimexDatalinkClient::Protocol3::Eeprom::Anniversary.new(
-    time: Time.parse(anniversary["time"]),
-    anniversary: anniversary["anniversary"]
-  )
+anniversaries = []
+if data["anniversaries"] && !options[:no_anniversaries]
+  anniversaries = data["anniversaries"].map do |anniversary|
+    TimexDatalinkClient::Protocol3::Eeprom::Anniversary.new(
+      time: Time.parse(anniversary["time"]),
+      anniversary: anniversary["anniversary"]
+    )
+  end
 end
 
 # Create phone numbers
-phone_numbers = data["phone_numbers"].map do |phone_number|
-  TimexDatalinkClient::Protocol3::Eeprom::PhoneNumber.new(
-    name: phone_number["name"],
-    number: phone_number["number"],
-    type: phone_number["type"]
-  )
+phone_numbers = []
+if data["phone_numbers"] && !options[:no_phone_numbers]
+  phone_numbers = data["phone_numbers"].map do |phone_number|
+    TimexDatalinkClient::Protocol3::Eeprom::PhoneNumber.new(
+      name: phone_number["name"],
+      number: phone_number["number"],
+      type: phone_number["type"]
+    )
+  end
 end
 
 # Create lists
-lists = data["lists"].map do |list|
-  TimexDatalinkClient::Protocol3::Eeprom::List.new(
-    list_entry: list["list_entry"],
-    priority: list["priority"]
-  )
+lists = []
+if data["lists"] && !options[:no_lists]
+  lists = data["lists"].map do |list|
+    TimexDatalinkClient::Protocol3::Eeprom::List.new(
+      list_entry: list["list_entry"],
+      priority: list["priority"]
+    )
+  end
 end
+
 time1 = Time.now
 time2 = time1.dup.utc
 
@@ -102,24 +140,26 @@ time_models = [
   ),
 ]
 
-alarms = data["alarms"].map do |alarm|
-  TimexDatalinkClient::Protocol3::Alarm.new(
-    number: alarm["number"],
-    audible: alarm["audible"],
-    time: Time.new(0, 1, 1, alarm["hour"], alarm["minute"]),
-    message: alarm["message"]
-  )
+# Create alarms
+alarms = []
+if data["alarms"] && !options[:no_alarms]
+  alarms = data["alarms"].map do |alarm|
+    TimexDatalinkClient::Protocol3::Alarm.new(
+      number: alarm["number"],
+      audible: alarm["audible"],
+      time: Time.new(0, 1, 1, alarm["hour"], alarm["minute"]),
+      message: alarm["message"]
+    )
+  end
 end
 
 # Read SoundOptions and appointment_notification_minutes from JSON
-sound_options_data = data["sound_options"]
+sound_options_data = data["sound_options"] || {}
 sound_options = TimexDatalinkClient::Protocol3::SoundOptions.new(
-  hourly_chime: sound_options_data["hourly_chime"],
-  button_beep: sound_options_data["button_beep"]
+  hourly_chime: sound_options_data.fetch("hourly_chime", false),
+  button_beep: sound_options_data.fetch("button_beep", false)
 )
-appointment_notification_minutes = data["appointment_notification_minutes"]
-
-# Rest of your code to create appointments, anniversaries, etc.
+appointment_notification_minutes = data["appointment_notification_minutes"] || 0
 
 models = [
   TimexDatalinkClient::Protocol3::Sync.new,
